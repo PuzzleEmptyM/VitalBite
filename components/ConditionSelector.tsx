e7bc8571cedda7961944ced4b8ed6f41b81dd5bd
Moonwalkert3ch <6620@holbertonstudents.com>
2024-11-20 12:54:33 -0600

    changed bg to forest green for hover on condition buttons

--- components/ConditionSelector.tsx
+++ components/ConditionSelector.tsx
@@ -14,7 +14,7 @@ const ConditionSelector: React.FC = () => (
     {conditions.map((condition, index) => (
       <button
         key={index}
-        className="py-2 px-4 border border-gray-300 shadow-md text-teal font-playfair font-normal rounded-md hover:bg-teal hover:text-white"
+        className="py-2 px-4 border border-gray-300 shadow-md text-teal font-playfair font-normal rounded-md hover:bg-forest_green hover:text-white"
       >
         {condition}
       </button>
