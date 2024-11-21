0bb2876c2e284ea11c1e495bee7cfa0234ddbe52
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-11-21 15:50:20 -0600

    link back to login from signup

--- components/DisclaimerFooter.tsx
+++ components/DisclaimerFooter.tsx
@@ -1,7 +1,7 @@
 
 export default function DisclaimerFooter() {
     return (
-      <footer className="bottom-0 w-full mt-6">
+      <footer className="bottom-0 w-full mt-4">
         <p className="text-center text-forest_green font-playfair">Disclaimer</p>
       </footer>
     );
