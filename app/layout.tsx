a20c047d0308f6d7ad358297bfb59b44036c3b61
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-11-25 15:06:51 -0600

    made more compatibility for mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,19 @@ type Props = {
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body className={`antialiased  bg-[#00003c] text-white`}>
+      <head>
+        {/* Link to the web manifest */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* Meta tags for PWA compatibility on iOS */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+
+        {/* Theme color for browser UI */}
+        <meta name="theme-color" content="#1ED2AF" />
+      </head>
+      <body className={`antialiased bg-[#00003c] text-white`}>
         <SessionProvider>{children}</SessionProvider>
       </body>
     </html>
