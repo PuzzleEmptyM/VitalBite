09213d14b95b99871348a9c64f31d49b04b5cacf
cpmoore0317 <cpmoore317@gmail.com>
2024-11-25 14:08:12 -0600

    Updated footer for page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { useSession, signIn } from "next-auth/react";
+import FooterNavigation from "@/components/FooterNavigation";
 
 const Page = () => {
   const { data: session, status } = useSession();
@@ -156,23 +157,9 @@ const Page = () => {
         </div>
       </section>
 
-      <div className="mb-12"></div>
-
-      {/* Bottom Navigation */}
-      <nav className="fixed bottom-0 left-0 right-0 bg-cream flex justify-around py-2 shadow-md border-2 border-forest_green">
-        <button onClick={() => alert("Chatbot!")}>
-          <img src="/icons/chat.svg" alt="Chat" className="w-8 h-8" />
-        </button>
-        <button onClick={() => alert("Profile!")}>
-          <img src="/icons/user.svg" alt="Profile" className="w-8 h-8" />
-        </button>
-        <button onClick={() => alert("Home!")}>
-          <img src="/icons/home.svg" alt="Home" className="w-8 h-8" />
-        </button>
-        <button onClick={() => alert("Ingredient analyzer!")}>
-          <img src="/icons/carrot.svg" alt="Carrot" className="w-8 h-8" />
-        </button>
-      </nav>
+      <div className="mb-12">
+        <FooterNavigation />
+      </div>
     </div>
   );
 };
